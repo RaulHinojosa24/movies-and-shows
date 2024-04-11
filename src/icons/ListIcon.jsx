92db3543814fc391651c9eba29ccbdd46bb21e3a
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-04-11 21:44:47 +0200

    Icons updated

--- src/icons/ListIcon.jsx
+++ src/icons/ListIcon.jsx
@@ -1,3 +1,3 @@
 export default function ListIcon ({ className = '' }) {
-  return <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='2.75 2.75 18.5 18.5' className={className}><path d='M3.5 20.5L3.5 13.5L20.5 13.5V20.5H3.5Z' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' /><path d='M3.5 10.5L3.5 3.5L20.5 3.5V10.5L3.5 10.5Z' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' /></svg>
+  return <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' fill='none' viewBox='2.75 2.75 18.5 18.5' className={className}><path d='M3.5 20.5L3.5 13.5L20.5 13.5V20.5H3.5Z' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' /><path d='M3.5 10.5L3.5 3.5L20.5 3.5V10.5L3.5 10.5Z' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' /></svg>
 }
