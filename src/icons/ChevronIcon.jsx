e690f293e0b4b7b2d0667bcc4da6c7712f4c8dcf
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-04-15 17:17:36 +0200

    A couple of icons

---
+++ src/icons/ChevronIcon.jsx
@@ -0,0 +1,3 @@
+export default function ChevronIcon ({ className = '' }) {
+  return <svg className={className} width='1em' height='1em' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M15 6L9 12L15 18' stroke='currentColor' strokeWidth='3' strokeLinecap='round' strokeLinejoin='round' /> </svg>
+}
