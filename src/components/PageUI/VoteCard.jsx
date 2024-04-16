c9faf50dff605860f8923ef7531175b6be49abcb
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-04-16 13:51:54 +0200

    Vote card fixed to display perventage even with on vote count

--- src/components/PageUI/VoteCard.jsx
+++ src/components/PageUI/VoteCard.jsx
@@ -1,14 +1,14 @@
 import { useRef } from 'react'
 import { formatNumberSymbols, generateVoteColor, roundDecimals } from '../../utils/utility'
 
-export default function VoteCard ({ rating, count, small, minimal, width, className, ...props }) {
+export default function VoteCard ({ rating, count, small, minimal, width, className, noCount, ...props }) {
   const svgRef = useRef()
 
   if (minimal) {
     return <span className='font-bold px-2 rounded mr-2 custom-shadow-small' {...props}>{roundDecimals(rating, 0)} &#9733;</span>
   }
 
-  const prettyAvarage = count ? roundDecimals(rating * 10, 0) : 'NR'
+  const prettyAvarage = (count || noCount) ? roundDecimals(rating * 10, 0) : 'NR'
   const prettyCount = formatNumberSymbols(count, 1)
   const avarageColor = generateVoteColor(rating / 10)
 
@@ -25,18 +25,18 @@ export default function VoteCard ({ rating, count, small, minimal, width, classN
           viewBox='0 0 100 100' width='100%' height='100%' fill='none'
         >
           <circle cx='50' cy='50' r='42' stroke='#222' />
-          {count &&
+          {(count || noCount) &&
             <circle
               cx='50' cy='50' r='42'
               style={{
-                strokeDasharray: count ? 42 * 2 * Math.PI * rating * 10 / 100 + ', 999' : ''
+                strokeDasharray: (count || noCount) ? 42 * 2 * Math.PI * rating * 10 / 100 + ', 999' : ''
               }}
               stroke={avarageColor}
             />}
         </svg>
         <div className='flex items-start'>
           {prettyAvarage}
-          {count > 0 &&
+          {(count || noCount) > 0 &&
             <span className={small ? 'text-[0.6rem] -mt-[0.1rem]' : 'text-sm mt-1'}>%</span>}
         </div>
       </div>
