2ff366b318c6477c2205e740c6b3da1ed475c69a
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-04-17 13:06:36 +0200

    Change on tv layout and new custom compact header

---
+++ src/components/TvSeasons/SeasonHeaderCompact.jsx
@@ -0,0 +1,15 @@
+import { useRouteLoaderData } from 'react-router-dom'
+
+import HeaderCompact from '../PageUI/HeaderCompact'
+
+export default function SeasonHeaderCompact () {
+  const {
+    poster_path: posterPath,
+    name
+  } = useRouteLoaderData('season-details')
+  const { id } = useRouteLoaderData('tv-details')
+
+  return (
+    <HeaderCompact target={'/tv/' + id + '/season'} mediaType='tv' posterPath={posterPath} title={name} />
+  )
+}
--- src/layout/TvDetailsLayout.jsx
+++ src/layout/TvDetailsLayout.jsx
@@ -2,14 +2,17 @@ import { Outlet, useMatch } from 'react-router-dom'
 import { getTvDetails } from '../utils/http'
 import TvHeader from '../components/TvGeneral/TvHeader'
 import TvHeaderCompact from '../components/TvGeneral/TvHeaderCompact'
+import SeasonHeaderCompact from '../components/TvSeasons/SeasonHeaderCompact'
 
 export default function TvDetailsLayout () {
   const isRootPage = useMatch('/tv/:id')
+  const isSeasonPage = useMatch('/tv/:id/season/:season')
 
   return (
     <section>
       {isRootPage && <TvHeader />}
-      {!isRootPage && <TvHeaderCompact />}
+      {!isRootPage && !isSeasonPage && <TvHeaderCompact />}
+      {!isRootPage && isSeasonPage && <SeasonHeaderCompact />}
       <Outlet />
     </section>
   )
