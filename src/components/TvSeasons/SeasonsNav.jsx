fc68087c0df473584f4f6a01ff7205fdf770a24a
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-04-17 13:29:38 +0200

    Navigation between seasons

---
+++ src/components/TvSeasons/SeasonsNav.jsx
@@ -0,0 +1,26 @@
+import { Link, useParams, useRouteLoaderData } from 'react-router-dom'
+import ChevronIcon from '../../icons/ChevronIcon'
+
+export default function SeasonsNav () {
+  const { seasons } = useRouteLoaderData('tv-details')
+  const { season: currentSeasonNumber, id: tvID } = useParams()
+  const currentSeasonIndex = seasons.findIndex(s => String(s.season_number) === currentSeasonNumber)
+  const prevSeason = seasons[currentSeasonIndex - 1]
+  console.log(prevSeason)
+  const nextSeason = seasons[currentSeasonIndex + 1]
+  console.log(nextSeason)
+  return (
+    <section className='flex'>
+      {prevSeason &&
+        <Link className='font-semibold flex items-center gap-1' to={`/tv/${tvID}/season/${prevSeason.season_number}`}>
+          <ChevronIcon />
+          {prevSeason.name}
+        </Link>}
+      {nextSeason &&
+        <Link className='font-semibold flex items-center gap-1 ml-auto' to={`/tv/${tvID}/season/${nextSeason.season_number}`}>
+          {nextSeason.name}
+          <ChevronIcon className='rotate-180' />
+        </Link>}
+    </section>
+  )
+}
--- src/pages/TvSeasonGeneral.jsx
+++ src/pages/TvSeasonGeneral.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom'
 import Main from '../components/PageUI/Main'
 import { getTvSeasonDetails } from '../utils/http'
 import EpisodeList from '../components/TvSeasons/EpisodeList'
+import SeasonsNav from '../components/TvSeasons/SeasonsNav'
 
 export default function TvSeasonGeneralPage () {
   return (
     <Main
       center={
         <>
+          <SeasonsNav />
           <Link to='media' className='font-bold'>TODO: Ver Media</Link>
           <EpisodeList />
         </>
