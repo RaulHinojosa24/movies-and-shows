1f12c59bf30b3a0909e920e340e5c80fbf34abed
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-05-07 17:52:37 +0200

    Styling on colapsible ui element

--- src/components/UI/Colapsible.jsx
+++ src/components/UI/Colapsible.jsx
@@ -2,9 +2,10 @@ export default function Colapsible ({ title, subtitle, children, className = '' 
   return (
     <details className={'pt-2 px-4 open:py-2 open:px-4 group ' + className}>
       <summary className='font-semibold cursor-pointer -mt-2 -mx-4 py-2 px-4 group-open:mb-2 group-open:border-b-1 border-neutral-700/50'>
-        {title}
-        {subtitle &&
-          <span className='font-normal opacity-75'> &bull; {subtitle}</span>}
+        <div>{title}
+          {subtitle &&
+            <span className='font-normal text-neutral-500'> &bull; {subtitle}</span>}
+        </div>
       </summary>
       {children}
     </details>
