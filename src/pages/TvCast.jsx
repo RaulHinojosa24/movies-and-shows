daaffbfb1fe33fb1c745e817e97bc9015da4d294
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-05-25 13:34:26 +0200

    tv media and cast page with skeletons

--- src/pages/TvCast.jsx
+++ src/pages/TvCast.jsx
@@ -1,29 +1,53 @@
-import { useRouteLoaderData } from 'react-router-dom'
+import { Await, useRouteLoaderData } from 'react-router-dom'
 import CastList from '../components/Cast/CastList'
 import CrewList from '../components/Cast/CrewList'
 
 import Main from '../components/PageUI/Main'
 import { setDocTitle } from '../utils/utility'
+import { Suspense } from 'react'
+import CastItemSkeleton from '../components/Skeletons/CastItemSkeleton'
 
 export default function TvCastPage () {
-  const {
-    name,
-    original_name: originalName,
-    aggregate_credits: credits
-  } = useRouteLoaderData('tv-details')
-
-  setDocTitle(`${name || originalName} - Reparto y equipo`)
-
-  const { cast, crew } = credits
+  const { data: loaderTvDetails } = useRouteLoaderData('tv-details')
 
   return (
     <Main
       center={
-        <div className='grid md:grid-cols-2 gap-8'>
-          <CastList cast={cast} />
-          <CrewList crew={crew} />
-        </div>
+        <Suspense fallback={<Fallback />}>
+          <Await resolve={loaderTvDetails}>
+            {({
+              name,
+              original_name: originalName,
+              aggregate_credits: { cast, crew }
+            }) => {
+              setDocTitle(`${name || originalName} - Reparto y equipo`)
+
+              return (
+                <div className='grid md:grid-cols-2 gap-8'>
+                  <CastList cast={cast} />
+                  <CrewList crew={crew} />
+                </div>
+              )
+            }}
+          </Await>
+        </Suspense>
+
       }
     />
+  )
+}
+
+function Fallback () {
+  return (
+    <div className='grid md:grid-cols-2 gap-8'>
+      <div className='space-y-3'>
+        <div className='skeleton__title w-1/4' />
+        {Array(5).fill().map((_, i) => <CastItemSkeleton key={i} />)}
+      </div>
+      <div className='space-y-3'>
+        <div className='skeleton__title w-1/4' />
+        {Array(5).fill().map((_, i) => <CastItemSkeleton key={i} />)}
+      </div>
+    </div>
   )
 }
--- src/pages/TvMedia.jsx
+++ src/pages/TvMedia.jsx
@@ -1,35 +1,45 @@
-import { useRouteLoaderData } from 'react-router-dom'
+import { Await, useRouteLoaderData } from 'react-router-dom'
 import Main from '../components/PageUI/Main'
 import { setDocTitle } from '../utils/utility'
 import MediaBackdrops from '../components/Media/MediaBackdrops'
 import MediaPosters from '../components/Media/MediaPosters'
 import MediaVideos from '../components/Media/MediaVideos'
+import { Suspense } from 'react'
 
 export default function TvMediaPage () {
-  const {
-    name,
-    original_name: originalName,
-    images: {
-      backdrops,
-      posters
-    },
-    videos: {
-      results: videos
-    }
-  } = useRouteLoaderData('tv-details')
-
-  setDocTitle(`${name || originalName} - Fotos y vídeos`)
+  const { data: loaderTvDetails } = useRouteLoaderData('tv-details')
 
   return (
-    <Main center={
-      <>
-        {backdrops.length > 0 &&
-          <MediaBackdrops images={backdrops} />}
-        {posters.length > 0 &&
-          <MediaPosters images={posters} />}
-        {videos.length > 0 &&
-          <MediaVideos videos={videos} />}
-      </>
+    <Main
+      center={
+        <Suspense>
+          <Await resolve={loaderTvDetails}>
+            {({
+              name,
+              original_name: originalName,
+              images: {
+                backdrops,
+                posters
+              },
+              videos: {
+                results: videos
+              }
+            }) => {
+              setDocTitle(`${name || originalName} - Fotos y vídeos`)
+
+              return (
+                <>
+                  {backdrops.length > 0 &&
+                    <MediaBackdrops images={backdrops} />}
+                  {posters.length > 0 &&
+                    <MediaPosters images={posters} />}
+                  {videos.length > 0 &&
+                    <MediaVideos videos={videos} />}
+                </>
+              )
+            }}
+          </Await>
+        </Suspense>
       }
     />
   )
