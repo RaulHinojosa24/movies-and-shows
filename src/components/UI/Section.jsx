e3a4d171f1bf42ce46f7842b7953ddb178c0552c
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-06-17 17:13:56 +0200

    Added forwardRef on Section component

--- src/components/UI/Section.jsx
+++ src/components/UI/Section.jsx
@@ -1,9 +1,13 @@
-export default function Section ({ title, children, className = '' }) {
+import { forwardRef } from 'react'
+
+const Section = forwardRef(function Section ({ title, children, className = '' }, ref) {
   return (
-    <section className={`${className}`}>
+    <section className={`${className}`} ref={ref}>
       {title &&
         <h2 className='text-2xl text-yellow-400 font-bold mb-4'>{title}</h2>}
       {children}
     </section>
   )
-}
+})
+
+export default Section
