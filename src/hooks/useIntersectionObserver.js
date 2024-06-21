02f1e773830e213ea15b1361d0cda1676f384aba
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-06-21 17:04:21 +0200

    intersection observer persistence working

--- src/hooks/useIntersectionObserver.js
+++ src/hooks/useIntersectionObserver.js
@@ -1,21 +1,23 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 
-export default function useIntersectionObserver (element, rootMargin, permanent) {
-  const [isVisible, setState] = useState(false)
+export default function useIntersectionObserver (element, rootMargin, persistence) {
+  const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
     const current = element?.current
     const observer = new IntersectionObserver(
       ([entry]) => {
-        if (permanent && isVisible) return
-        setState(entry.isIntersecting)
+        setIsVisible(entry.isIntersecting)
+        if (entry.isIntersecting && persistence) {
+          observer.unobserve(current)
+        }
       },
       { rootMargin }
     )
     current && observer?.observe(current)
 
     return () => current && observer.unobserve(current)
-  }, [])
+  }, [element, isVisible, persistence, rootMargin])
 
   return isVisible
 }
