32caf33fec974eb940b6fd2aa2eeb7d176be67b2
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-07-25 18:28:23 +0200

    Modal component update

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -2,7 +2,7 @@ import { createPortal } from 'react-dom'
 import CrossIcon from '../../icons/CrossIcon'
 import { useEffect } from 'react'
 
-export default function Modal ({ className = '', children, onClose, crossInside }) {
+export default function Modal ({ className = '', children, onClose, crossInside, noCross, allBlack }) {
   useEffect(() => {
     const handler = (event) => {
       if (event.key === 'Escape') onClose()
@@ -19,19 +19,19 @@ export default function Modal ({ className = '', children, onClose, crossInside 
   return (
     <>
       {createPortal(
-        <div className='fixed top-0 left-0 w-screen h-screen z-30 bg-neutral-900 bg-opacity-80 backdrop-blur-sm backdrop-grayscale animate-fade' />,
+        <div className={`fixed top-0 left-0 w-screen h-screen z-[95] dark:bg-neutral-900 bg-neutral-400 ${allBlack ? '' : '!bg-opacity-80'} backdrop-blur-sm animate-fade`} />,
         document.getElementById('backdrop'))}
       {createPortal(
         <>
-          <div className={`fixed max-h-screen overflow-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-40 ${className}`}>
+          <div className={`fixed max-h-screen overflow-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[100] ${className}`}>
             {children}
-            {crossInside &&
-              <button className='absolute h-4 aspect-square top-4 right-4 z-50' onClick={onClose}>
+            {crossInside && !noCross &&
+              <button className='absolute h-4 aspect-square top-4 right-4' onClick={onClose}>
                 <CrossIcon />
               </button>}
           </div>
-          {!crossInside &&
-            <button className='fixed top-[5vh] right-[5vh] h-6 aspect-square translate-x-1/2 -translate-y-1/2 z-50' onClick={onClose}>
+          {!crossInside && !noCross &&
+            <button className='fixed top-[5vh] right-[5vh] h-6 aspect-square translate-x-1/2 -translate-y-1/2 z-[105]' onClick={onClose}>
               <CrossIcon />
             </button>}
         </>, document.getElementById('modal'))}
