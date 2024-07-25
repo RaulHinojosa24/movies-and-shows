019fe1b1e3da0951d578aa729c84ceee20899350
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-07-25 18:26:29 +0200

    search bar changes for compatibilities

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,45 +1,53 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import LensIcon from '../../icons/LensIcon'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import CrossIcon from '../../icons/CrossIcon'
 
 export default function SearchBar ({ className = '', compact }) {
   const [searchParams] = useSearchParams()
-  const prevQuery = searchParams.get('query') || ''
-  const [query, setQuery] = useState(prevQuery)
+  const spQuery = searchParams.get('query') || ''
+  const [userInput, setUserInput] = useState(spQuery)
   const navigate = useNavigate()
   const [isCompact, setIsCompact] = useState(Boolean(compact))
   const inputRef = useRef()
 
   const searchHandler = (event) => {
-    event.preventDefault()
+    event?.preventDefault()
 
-    if (query.trim() && query !== prevQuery) {
-      navigate('/search/movie?query=' + query)
-      event.target.querySelector('input').blur()
+    if (userInput.trim() && userInput !== spQuery) {
+      navigate('/search/movie?query=' + userInput)
+      event?.target?.querySelector('input').blur()
     }
   }
 
+  useEffect(() => {
+    if (!isCompact) inputRef.current.focus()
+  }, [isCompact])
+
+  useEffect(() => {
+    setUserInput(searchParams.get('query') || '')
+  }, [searchParams])
+
   return (
-    <div className={'py-1 flex items-center gap-2 ' + className}>
-      <span onClick={compact && (isCompact ? () => setIsCompact(false) : () => setIsCompact(true))}>
-        {(isCompact || !compact) &&
-          <LensIcon className={'text-2xl ' + (isCompact ? 'cursor-pointer' : '')} />}
-        {!isCompact && compact &&
-          <CrossIcon className='text-sm cursor-pointer' />}
-      </span>
-      <form action='.' className={isCompact ? 'hidden w-0' : 'w-full'} onSubmit={searchHandler}>
+    <div className={'py-1 flex items-center justify-end transition-all ' + (compact ? 'relative ' : 'gap-2 ') + (isCompact ? '' : 'grow ') + className}>
+      <form className={(compact ? 'absolute left-0 ' : '') + (isCompact ? 'invisible w-0' : 'w-[calc(100%-1.5rem-0.5rem)]')} onSubmit={searchHandler}>
         <input
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
           type='text'
           name='query'
-          className='w-full py-2 px-4 border-none bg-transparent'
+          className='w-full py-2 px-2 border-none bg-transparent'
           placeholder={compact ? 'Búsqueda...' : 'Busca películas, series o gente...'}
-          autoFocus={isCompact || !query}
+          autoFocus={isCompact || !userInput}
           ref={inputRef}
         />
       </form>
+      <button onClick={compact ? (isCompact ? () => setIsCompact(false) : () => setIsCompact(true)) : () => searchHandler()}>
+        {(isCompact || !compact) &&
+          <LensIcon className='text-2xl' />}
+        {!isCompact && compact &&
+          <CrossIcon className='text-sm' />}
+      </button>
     </div>
   )
 }
