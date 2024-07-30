2ed443053d2fa9414cab3248530ff69c5460b08c
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-07-30 18:45:21 +0200

    General media component optimized

--- src/components/Media/GeneralMedia.jsx
+++ src/components/Media/GeneralMedia.jsx
@@ -5,62 +5,42 @@ import { retrieveConfig } from '../../utils/utility'
 import { useEffect, useState } from 'react'
 
 export default function GeneralMedia ({ title, backdrops = [], posters, videos = [], pageType, className = '' }) {
-  const trailer = videos.reduce((acc, curr) => {
-    if (acc.length === 0 && curr.type === 'Trailer') {
-      return [curr]
-    }
-    return acc
-  }, [])
+  const trailer = videos.find(el => el?.type === 'Trailer')
   const fourBackdrops = backdrops.slice(0, 4)
 
-  const wideEls = [...trailer, ...fourBackdrops]
+  const wideEls = trailer ? [trailer, ...fourBackdrops] : fourBackdrops
   const highEls = posters.slice(0, 8)
 
   if (pageType === 'colección') {
     return (
-      <Link to='media' className='mx-auto relative group'>
+      <section className='relative'>
         <div className='flex md:hidden justify-center items-center'>
-          {wideEls.map((el, i) => {
-            if (i >= 2) return null
-            return (
-              <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-1/2 first:rounded-l last:rounded-r' />
-            )
-          })}
+          {wideEls.slice(0, 2).map(el =>
+            <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-1/2 first:rounded-l last:rounded-r' />)}
         </div>
         <div className='hidden md:flex justify-center items-center'>
-          {wideEls.map((el, i) => {
-            if (i >= 4) return null
-            return (
-              <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-1/4 first:rounded-l last:rounded-r' />
-            )
-          })}
+          {wideEls.map(el =>
+            <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-1/4 first:rounded-l last:rounded-r' />
+          )}
         </div>
-        <span className='absolute top-0 left-0 w-full h-full grid text-center content-center font-semibold opacity-0 group-hover:opacity-100 transition-opacity bg-black/70 backdrop-blur-sm px-12 text-lg'>Ver todo el contenido multimedia</span>
-      </Link>
+        <Link to='media' className='no-underline absolute top-0 left-0 w-full h-full grid content-center font-semibold opacity-0 hover:opacity-100 transition-opacity bg-black/70 backdrop-blur-sm px-12 text-center text-lg text-white rounded'>Ver todo el contenido multimedia</Link>
+      </section>
     )
   }
 
   if (pageType === 'temporada') {
     return (
-      <Link to='media' className='mx-auto relative group'>
-        <div className='flex items-center sm:hidden relative'>
-          {highEls.map((poster, i) => {
-            if (i >= 4) return null
-            return (
-              <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-1/4 first:rounded-l last:rounded-r' />
-            )
-          })}
+      <section className='relative'>
+        <div className='flex sm:hidden justify-center items-center'>
+          {highEls.slice(0, 4).map(poster =>
+            <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-1/4 first:rounded-l last:rounded-r' />)}
         </div>
-        <div className='sm:flex items-center hidden relative'>
-          {highEls.map((poster, i) => {
-            if (i >= 8) return null
-            return (
-              <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[12.5%] first:rounded-l last:rounded-r' />
-            )
-          })}
+        <div className='sm:flex hidden justify-center items-center'>
+          {highEls.map(poster =>
+            <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[12.5%] first:rounded-l last:rounded-r' />)}
         </div>
-        <span className='absolute top-0 left-0 w-full h-full grid text-center content-center font-semibold opacity-0 group-hover:opacity-100 transition-opacity bg-black/70 backdrop-blur-sm px-12 text-lg'>Ver todo el contenido multimedia</span>
-      </Link>
+        <Link to='media' className='no-underline absolute top-0 left-0 w-full h-full grid content-center font-semibold opacity-0 hover:opacity-100 transition-opacity bg-black/70 backdrop-blur-sm px-12 text-center text-lg text-white rounded'>Ver todo el contenido multimedia</Link>
+      </section>
     )
   }
 
@@ -68,132 +48,56 @@ export default function GeneralMedia ({ title, backdrops = [], posters, videos =
     <>
       <Section title='Multimedia' className={'space-y-4 ' + className}>
         {wideEls.length >= 2 && highEls.length >= 1 &&
-          <>
-            <div className='flex sm:hidden md:flex lg:hidden items-center'>
-              <div className='flex flex-col w-[57.14%] rounded-l overflow-hidden'>
-                {wideEls.map((el, i) => {
-                  if (i > 1) return null
-                  if (el.type) {
-                    return <VideoModal key={el.id} video={el} />
-                  } else {
-                    return (
-                      <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' />
-                    )
-                  }
-                })}
-              </div>
-              {highEls.map((poster, i) => {
-                if (i >= 1) return null
-                return (
-                  <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[42.86%] rounded-r' />
-                )
-              })}
+          <div className='flex items-center overflow-hidden rounded'>
+            <div className='flex sm:contents md:flex lg:contents flex-col w-[57.14%] rounded-l overflow-hidden'>
+              {wideEls.slice(0, 2).map(el =>
+                el.type
+                  ? <VideoModal key={el.id} video={el} className='w-full sm:w-[42.1%] md:w-auto lg:w-[42.1%]' />
+                  : <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-full sm:w-[42.1%] md:w-full lg:w-[42.1%]' />
+              )}
             </div>
-            <div className='hidden sm:flex md:hidden lg:flex items-center [&>*:first-child]:rounded-l [&>*:last-child]:rounded-r'>
-              {wideEls.map((el, i) => {
-                if (i > 1) return null
-                if (el.type) {
-                  return <VideoModal key={el.id} video={el} className='w-[42.1%]' />
-                } else {
-                  return (
-                    <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-[42.1%]' />
-                  )
-                }
-              })}
-              {highEls.map((poster, i) => {
-                if (i >= 1) return null
-                return (
-                  <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[15.8%]' />
-                )
-              })}
-            </div>
-          </>}
+            <Image key={highEls[0].file_path} pageType={pageType} element={highEls[0]} title={title} mediaType='poster' className='w-[42.86%] sm:w-[15.8%] md:w-[42.86%] lg:w-[15.8%]' />
+          </div>}
+
         {wideEls.length >= 2 && highEls.length === 0 &&
           <>
-            <div className='flex sm:hidden md:flex lg:hidden flex-col items-center rounded overflow-hidden'>
-              {wideEls.map((el, i) => {
-                if (i >= 2) return null
-                if (el.type) {
-                  return <VideoModal key={el.id} video={el} className='w-full' />
-                } else {
-                  return (
-                    <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-full' />
-                  )
-                }
-              })}
-            </div>
-            <div className='hidden sm:flex md:hidden lg:flex items-center rounded overflow-hidden'>
-              {wideEls.map((el, i) => {
-                if (i >= 2) return null
-                if (el.type) {
-                  return <VideoModal key={el.id} video={el} className='w-1/2' />
-                } else {
-                  return (
-                    <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-1/2' />
-                  )
-                }
-              })}
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 items-center rounded overflow-hidden'>
+              {wideEls.slice(0, 2).map(el =>
+                el.type
+                  ? <VideoModal key={el.id} video={el} className='w-full' />
+                  : <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-full' />
+              )}
             </div>
           </>}
+
         {wideEls.length === 1 &&
-          <>
-            <div className='flex sm:hidden md:flex lg:hidden flex-col items-center [&>*:first-child]:rounded-t [&>*:last-child]:rounded-b'>
-              {wideEls[0].type
-                ? <VideoModal key={wideEls[0].id} video={wideEls[0]} className={`w-full ${highEls.length < 3 && 'rounded-br'}`} />
-                : <Image key={wideEls[0].file_path} pageType={pageType} element={wideEls[0]} title={title} mediaType='backdrop' className={`w-full ${highEls.length < 3 && 'rounded-br'}`} />}
-              {highEls.length > 0 &&
-                <div className='flex w-full overflow-hidden'>
-                  {highEls.map((poster, i) => {
-                    if (i >= 3) return null
-                    return (
-                      <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[33.33%] last:rounded-br' />
-                    )
-                  })}
-                </div>}
-            </div>
-            <div className='hidden sm:flex md:hidden lg:flex items-center [&>*:first-child]:rounded-l [&>*:last-child]:rounded-r'>
-              {wideEls[0].type
-                ? <VideoModal key={wideEls[0].id} video={wideEls[0]} className='w-[47%]' />
-                : <Image key={wideEls[0].file_path} pageType={pageType} element={wideEls[0]} title={title} mediaType='backdrop' className='w-[47%]' />}
-              {highEls.map((poster, i) => {
-                if (i >= 3) return null
-                return (
-                  <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[17.66%]' />
-                )
-              })}
-            </div>
-          </>}
+          <div className='flex flex-col sm:flex-row md:flex-col lg:flex-row items-center overflow-hidden rounded'>
+            {wideEls[0].type
+              ? <VideoModal key={wideEls[0].id} video={wideEls[0]} className={`shrink-0 w-full sm:w-[47%] md:w-full lg:w-[47%] ${!highEls.length && 'rounded'} ${highEls.length && highEls.length < 3 && 'rounded-br sm:rounded-none md:rounded-br lg:rounded-none'}`} />
+              : <Image key={wideEls[0].file_path} pageType={pageType} element={wideEls[0]} title={title} mediaType='backdrop' className={`shrink-0 w-full sm:w-[47%] md:w-full lg:w-[47%] ${!highEls.length && 'rounded'} ${highEls.length && highEls.length < 3 && 'rounded-br sm:rounded-none md:rounded-br lg:rounded-none'}`} />}
+            {highEls.length > 0 &&
+              <div className='flex w-full'>
+                {highEls.slice(0, 3).map(poster =>
+                  <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[33.33%] last:rounded-br last:rounded-tr-none sm:last:rounded-tr md:last:rounded-tr-none lg:last:rounded-tr' />)}
+              </div>}
+          </div>}
+
         {wideEls.length === 0 && highEls.length > 0 &&
-          <>
-            <div className='flex sm:hidden md:flex lg:hidden flex-col items-center [&>*:first-child]:rounded-t [&>*:last-child]:rounded-b'>
-              {highEls.length > 0 &&
-                <div className='flex w-full overflow-hidden'>
-                  {highEls.map((poster, i) => {
-                    if (i >= 3) return null
-                    return (
-                      <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className={`w-[33.33%] ${highEls.length < 6 && 'last:rounded-r'}`} />
-                    )
-                  })}
-                </div>}
-              {highEls.length > 3 &&
-                <div className='flex w-full overflow-hidden'>
-                  {highEls.map((poster, i) => {
-                    if (i < 3 || i >= 6) return null
-                    return (
-                      <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[33.33%] last:rounded-br' />
-                    )
-                  })}
-                </div>}
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 rounded overflow-hidden'>
+            <div className='w-full grid grid-cols-3'>
+              {highEls.slice(0, 3).map(poster =>
+                <Image
+                  key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className={`w-full ${highEls.length <= 3 && 'last:rounded-r'} ${highEls.length < 6 && highEls.length > 3 && 'last:rounded-br sm:last:rounded-br-none md:last:rounded-br lglast:rounded-br-none'} `}
+                />)}
             </div>
-            <div className='hidden sm:flex md:hidden lg:flex items-center'>
-              {highEls.map((poster, i) => {
-                if (i >= 6) return null
-                return (
-                  <Image key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-[16.66%] first:rounded-l last:rounded-r' />
-                )
-              })}
+            <div className='w-full grid grid-cols-3'>
+              {highEls.slice(3, 6).map(poster =>
+                <Image
+                  key={poster.file_path} pageType={pageType} element={poster} title={title} mediaType='poster' className='w-full last:rounded-br sm:last:rounded-r md:last:rounded-br lg:last:rounded-r'
+                />)}
             </div>
-          </>}
+          </div>}
+
         {wideEls.length === 0 && highEls.length === 0
           ? <p className='italic'>No disponemos de material multimedia.</p>
           : <Link to='media' className='mt-2 inline-block'>Ver todo el contenido multimedia</Link>}
