645a10898db9285f39d13027d7b71832f3fc93ee
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-08-24 11:09:07 +0200

    go to top button portaled to overlay

--- src/components/PageUI/GoToTopButton.jsx
+++ src/components/PageUI/GoToTopButton.jsx
@@ -1,14 +1,21 @@
+import { createPortal } from 'react-dom'
 import ArrowUp from '../../icons/ArrowUpIcon'
 
 export default function GoToTopButton () {
   const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
   return (
-    <button
-      id='scroll-to-top'
-      className='fixed bottom-8 right-8 w-12 bg-yellow-400 aspect-square rounded-full grid place-items-center z-20'
-      onClick={scrollToTop}
-    >
-      <ArrowUp className='h-1/2' />
-    </button>
+    <>
+      {createPortal(
+        <button
+          id='scroll-to-top'
+          className='fixed bottom-8 right-8 w-12 bg-yellow-400 aspect-square rounded-full grid place-items-center z-20'
+          onClick={scrollToTop}
+        >
+          <ArrowUp className='h-1/2' />
+        </button>
+        , document.getElementById('overlay'))}
+    </>
+
   )
 }
