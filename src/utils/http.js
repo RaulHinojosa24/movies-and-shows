c1d76d352ebd1071bd5af500876f038ef0ab255e
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-09-02 17:12:21 +0200

    All http calls endpoints adapted to the new backend strucutre

--- src/utils/http.js
+++ src/utils/http.js
@@ -9,10 +9,21 @@ const POST_OPTIONS = {
   method: 'POST'
 }
 
-async function sendRequest (url, delay) {
+async function sendRequest (path, params = {}, delay = 0) {
+  const url = new URL(`${API_URL}${path}`)
+
+  for (const key in params) {
+    if (Object.prototype.hasOwnProperty.call(params, key)) {
+      const element = params[key]
+      if (element !== null) {
+        url.searchParams.append(key, element)
+      }
+    }
+  }
+
   return new Promise(resolve => setTimeout(resolve, delay)).then(async () => {
     try {
-      const response = await fetch(url)
+      const response = await fetch(url.href)
 
       if (!response.ok) {
         const error = new Error('Network response was not ok')
@@ -34,23 +45,23 @@ async function sendRequest (url, delay) {
 }
 
 export function getAPIConfiguration () {
-  return sendRequest(API_URL + '/3/configuration?append_to_response=countries,jobs,languages,timezones')
+  return sendRequest('/get-api-configuration')
 }
 
 export function getMovieGenres () {
-  return sendRequest(API_URL + '/3/genre/movie/list?language=' + language)
+  return sendRequest('/get-movie-genres')
 }
 
 export function getTvGenres () {
-  return sendRequest(API_URL + '/3/genre/tv/list?language=' + language)
+  return sendRequest('/get-tv-genres')
 }
 
 export function getMovieProviders () {
-  return sendRequest(`${API_URL}/3/watch/providers/movie?language=${language}&watch_region=${region}`)
+  return sendRequest('/get-movie-providers')
 }
 
 export function getTvProviders () {
-  return sendRequest(`${API_URL}/3/watch/providers/tv?language=${language}&watch_region=${region}`)
+  return sendRequest('/get-tv-providers')
 }
 
 export function getRequestToken () {
@@ -69,61 +80,66 @@ export function createSessionID (requestToken) {
 }
 
 export function getMovieDetails (id) {
-  return sendRequest(`${API_URL}/3/movie/${id}?language=${language}&include_image_language=null&append_to_response=images,videos,keywords,lists,recommendations,reviews,watch/providers,release_dates,external_ids,credits`)
+  return sendRequest('/get-movie-details', {
+    id
+  })
 }
 
 export function getCollectionDetails (id) {
-  return sendRequest(`${API_URL}/3/collection/${id}?language=${language}&include_image_language=null&append_to_response=images`)
+  return sendRequest('/get-collection-details', {
+    id
+  })
 }
 
 export function getPersonDetails (id) {
-  return sendRequest(`${API_URL}/3/person/${id}?language=${language}&append_to_response=combined_credits,external_ids,images`)
+  return sendRequest('/get-person-details', {
+    id
+  })
 }
 
 export function getTvDetails (id) {
-  return sendRequest(`${API_URL}/3/tv/${id}?language=${language}&append_to_response=aggregate_credits,content_ratings,external_ids,images,keywords,lists,recommendations,reviews,videos,watch/providers&include_image_language=null`)
+  return sendRequest('/get-tv-details', {
+    id
+  })
 }
 
 export async function getListDetails (id, page = 1) {
-  return sendRequest(`${API_URL}/4/list/${id}?language=${language}&page=${page}`)
+  return sendRequest('/get-list-details', {
+    id,
+    page
+  })
 }
 
 export function getMoviesByQuery (query, page = 1) {
-  const moviesUrl = new URL(API_URL + '/3/search/movie')
-  moviesUrl.searchParams.append('query', query)
-  moviesUrl.searchParams.append('language', language)
-  moviesUrl.searchParams.append('include_adult', false)
-  moviesUrl.searchParams.append('page', page)
-
-  return sendRequest(moviesUrl.href)
+  return sendRequest('/search-movies', {
+    query,
+    page
+  })
 }
 
 export function getTvByQuery (query, page = 1) {
-  const seriesUrl = new URL(API_URL + '/3/search/tv')
-  seriesUrl.searchParams.append('query', query)
-  seriesUrl.searchParams.append('language', language)
-  seriesUrl.searchParams.append('include_adult', false)
-  seriesUrl.searchParams.append('page', page)
-
-  return sendRequest(seriesUrl.href)
+  return sendRequest('/search-tvs', {
+    query,
+    page
+  })
 }
 
 export function getPeopleByQuery (query, page = 1) {
-  const personUrl = new URL(API_URL + '/3/search/person')
-  personUrl.searchParams.append('query', query)
-  personUrl.searchParams.append('language', language)
-  personUrl.searchParams.append('include_adult', false)
-  personUrl.searchParams.append('page', page)
-
-  return sendRequest(personUrl.href)
+  return sendRequest('/search-people', {
+    query,
+    page
+  })
 }
 
-export function getTvSeasonDetails (tvID, season) {
-  return sendRequest(`${API_URL}/3/tv/${tvID}/season/${season}?language=${language}&include_image_language=en,null&include_video_language=en,null&append_to_response=account_states,aggregate_credits,images,videos`)
+export function getTvSeasonDetails (tv, season) {
+  return sendRequest('/get-tv-season-details', {
+    tv,
+    season
+  })
 }
 
 export function getKeywordsByQuery (query, page = 1) {
-  const url = new URL(API_URL + '/3/search/keyword')
+  const url = new URL(`${API_URL}/search-keywords`)
   url.searchParams.append('query', query)
   url.searchParams.append('page', page)
 
@@ -147,26 +163,23 @@ export function discoverMovies ({
   keywords,
   watchProviders
 }) {
-  const url = new URL(API_URL + '/3/discover/movie')
-  url.searchParams.append('page', page || 1)
-  url.searchParams.append('include_video', true)
-  url.searchParams.append('language', language)
-  url.searchParams.append('watch_region', region)
-  url.searchParams.append('include_adult', includeAdult || false)
-  url.searchParams.append('sort_by', `${sortBy || 'popularity'}.${sortDirection || 'desc'}`)
-  url.searchParams.append('with_watch_monetization_types', watchTypes || '')
-  url.searchParams.append('vote_count.gte', voteCount || '')
-  url.searchParams.append('vote_average.gte', voteMin || '')
-  url.searchParams.append('vote_average.lte', voteMax || '')
-  url.searchParams.append('with_runtime.gte', durationMin || '')
-  url.searchParams.append('with_runtime.lte', durationMax || '')
-  url.searchParams.append('release_date.gte', fromDate || '')
-  url.searchParams.append('release_date.lte', toDate || '')
-  url.searchParams.append('with_genres', genres || '')
-  url.searchParams.append('with_keywords', keywords ? keywords.split('|').map(kw => kw.split('%')[0]).join('|') : '')
-  url.searchParams.append('with_watch_providers', watchProviders || '')
-
-  return sendRequest(url.href)
+  return sendRequest('/discover-movies', {
+    page,
+    include_adult: includeAdult,
+    sort_by: sortBy,
+    sort_direction: sortDirection,
+    watch_types: watchTypes,
+    vote_count: voteCount,
+    vote_min: voteMin,
+    vote_max: voteMax,
+    duration_min: durationMin,
+    duration_max: durationMax,
+    from_date: fromDate,
+    to_date: toDate,
+    genres,
+    keywords,
+    watch_providers: watchProviders
+  })
 }
 
 export function discoverTvs ({
@@ -186,43 +199,44 @@ export function discoverTvs ({
   keywords,
   watchProviders
 }) {
-  const url = new URL(API_URL + '/3/discover/tv')
-  url.searchParams.append('page', page || 1)
-  url.searchParams.append('language', language)
-  url.searchParams.append('watch_region', region)
-  url.searchParams.append('include_adult', includeAdult || false)
-  url.searchParams.append('sort_by', `${sortBy || 'popularity'}.${sortDirection || 'desc'}`)
-  url.searchParams.append('with_watch_monetization_types', watchTypes || '')
-  url.searchParams.append('vote_count.gte', voteCount || '')
-  url.searchParams.append('vote_average.gte', voteMin || '')
-  url.searchParams.append('vote_average.lte', voteMax || '')
-  url.searchParams.append('with_runtime.gte', durationMin || '')
-  url.searchParams.append('with_runtime.lte', durationMax || '')
-  url.searchParams.append('first_air_date.gte', fromDate || '')
-  url.searchParams.append('first_air_date.lte', toDate || '')
-  url.searchParams.append('with_genres', genres || '')
-  url.searchParams.append('with_keywords', keywords ? keywords.split('|').map(kw => kw.split('%')[0]).join('|') : '')
-  url.searchParams.append('with_watch_providers', watchProviders || '')
-
-  return sendRequest(url.href)
+  return sendRequest('/discover-tvs', {
+    page,
+    include_adult: includeAdult,
+    sort_by: sortBy,
+    sort_direction: sortDirection,
+    watch_types: watchTypes,
+    vote_count: voteCount,
+    vote_min: voteMin,
+    vote_max: voteMax,
+    duration_min: durationMin,
+    duration_max: durationMax,
+    from_date: fromDate,
+    to_date: toDate,
+    genres,
+    keywords,
+    watch_providers: watchProviders
+  })
 }
 
 export function getNowPlayingMovies () {
-  return sendRequest(`${API_URL}/3/movie/now_playing?language=${language}page=1&region=${region}`)
+  return sendRequest('/get-now-playing-movies')
 }
 
 export function getTrendingAll (timeWindow = 'day', page = 1) {
-  return sendRequest(`${API_URL}/3/trending/all/${timeWindow}?language=${language}&page=${page}`)
+  return sendRequest('/get-trending-all', {
+    time_window: timeWindow,
+    page
+  })
 }
 
 export function getPopularMovies () {
-  return sendRequest(`${API_URL}/3/movie/popular?language=${language}&page=1&region=${region}`)
+  return sendRequest('/get-popular-movies')
 }
 
 export function getPopularTvs () {
-  return sendRequest(`${API_URL}/3/tv/popular?language=${language}&page=1`)
+  return sendRequest('/get-popular-tvs')
 }
 
 export function getPopularPeople () {
-  return sendRequest(`${API_URL}/3/person/popular?language=${language}&page=1`)
+  return sendRequest('/get-popular-people')
 }
