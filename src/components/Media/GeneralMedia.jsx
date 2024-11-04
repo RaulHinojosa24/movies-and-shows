25c49fca537067bb89ce9cd755ce374e219df2bc
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-11-04 13:01:37 +0100

    General media sizing change

--- src/components/Media/GeneralMedia.jsx
+++ src/components/Media/GeneralMedia.jsx
@@ -11,23 +11,23 @@ export default function GeneralMedia ({ title, backdrops = [], posters, pageType
     <>
       <Section title='Multimedia' className={'space-y-4 ' + className}>
         <div className='relative flex flex-row rounded overflow-hidden'>
-          <div className='w-full flex flex-col sm:flex-row md:flex-col lg:flex-row'>
+          <div className='w-full flex flex-col sm:flex-row'>
             {wideEls.length > 0 &&
               wideEls.map(el =>
-                <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-full sm:w-1/2 md:w-full lg:w-1/2' />)}
+                <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='backdrop' className='w-full sm:w-1/2' />)}
             {wideEls.length <= 1 && highEls.length > 0 &&
-              <div className='flex w-full sm:w-1/2 md:w-full lg:w-1/2'>
+              <div className='flex w-full sm:w-1/2'>
                 {highEls.slice(0, 3).map(el =>
                   <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='poster' className='w-1/3' />)}
               </div>}
             {wideEls.length === 0 && highEls.length > 3 &&
-              <div className='flex w-full sm:w-1/2 md:w-full lg:w-1/2'>
+              <div className='flex w-full sm:w-1/2'>
                 {highEls.slice(3, 6).map(el =>
                   <Image key={el.file_path} pageType={pageType} element={el} title={title} mediaType='poster' className='w-1/3' />)}
               </div>}
           </div>
           {wideEls.length === 2 && highEls[0] &&
-            <div className='shrink-0 w-[42.86%] sm:w-[15.8%] md:w-[42.86%] lg:w-[15.8%]'>
+            <div className='shrink-0 w-[42.86%] sm:w-[15.8%]'>
               <Image key={highEls[0].file_path} pageType={pageType} element={highEls[0]} title={title} mediaType='poster' />
             </div>}
           <Link to='media' className='no-underline absolute top-0 left-0 w-full h-full grid content-center font-semibold opacity-0 hover:opacity-100 transition-opacity bg-black/70 backdrop-blur-sm px-12 text-center text-lg text-white rounded'>Ver todo el contenido multimedia</Link>
