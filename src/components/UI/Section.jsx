c124228c1c8e00f18a0d19c823491ad459a8e101
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-11-04 11:42:34 +0100

    Added subtitles on section and subsection components

--- src/components/UI/Section.jsx
+++ src/components/UI/Section.jsx
@@ -1,10 +1,14 @@
 import { forwardRef } from 'react'
 
-const Section = forwardRef(function Section ({ title, children, className = '' }, ref) {
+const Section = forwardRef(function Section ({ title, subtitle, children, className = '' }, ref) {
   return (
     <section className={`${className}`} ref={ref}>
       {title &&
-        <h2 className='text-2xl text-accent font-bold mb-4'>{title}</h2>}
+        <h2 className='text-2xl text-accent font-bold mb-4'>
+          {title}
+          {subtitle &&
+            <span className='font-semibold text-medium text-xl'> &bull; {subtitle}</span>}
+        </h2>}
       {children}
     </section>
   )
--- src/components/UI/SubSection.jsx
+++ src/components/UI/SubSection.jsx
@@ -1,8 +1,11 @@
-export default function SubSection ({ title, children, className = '' }) {
+export default function SubSection ({ title, subtitle, children, className = '' }) {
   return (
     <div className={className}>
       {title &&
-        <h3 className='font-semibold text-lg'>{title}</h3>}
+        <h3 className='font-semibold text-lg'>{title}
+          {subtitle &&
+            <span className='text-medium'> &bull; {subtitle}</span>}
+        </h3>}
       {children}
     </div>
   )
